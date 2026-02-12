f26a33199e43d0c0531f1fea871cd6057c37eb7a
Minibooga <silverclaw287@outlook.com>
2026-02-12 18:18:38 -0500

    Refactor login function for user authentication

--- allm.py
+++ allm.py
@@ -1,21 +1,71 @@
 import hashlib
+import os
+import json
+
+
+USERS_FILE = "users.json"
 
 def hash_password(password):
   return hashlib.sha256(password.encode()).hexdigest()
+
+def load_users():
+  """Load users from file"""
+  if os.path.exists(USERS_FILE):
+    try:
+      with open(USERS_FILE, "r") as f:
+        return json.load(f)
+      except:
+        return {}
+    return{}
+
+def save_users(users):
+  """Save users to file"""
+  with open(USERS_FILE, "w") as f:
+    json.dump(users, f, indent=4)
+
+
   
 def login():
   global usrname
   global cfp_hash
-  usrname=input("Please input your username: ")
-  pwd=input("Please input your password: ")
-  cfp=input("Reenter the password: ")
-  if cfp==pwd:
-    print("Thank you.")
-    cfp_hash=hash_password(cfp)
-    lockscreen()
+  users=load_users()
+
+  while True:
+    print("\n---LOGIN---")
+    print("1. Login")
+    print("2. Register")
+    option=input(Select:)
+
+    if choice=="1":
+      usrname = input("Enter your username: ")
+      pwd = input("Enter your password: ")
+
+      if usrname in users and hash_password(pwd) == users[usrname]:
+        cfp_hash=users[usrname]
+        print("Login succsessful.")
+        allmos()
+        break
+      else:
+        print("Invalid username or password. Did you have an account saved?")
+
+    elif choice == "2"
+      usrname=input("Please input your username: ")
+    if usrname in users:
+      print("User exists.")
+      continue
+
+    pwd=input("Please input your password: ")
+    cfp=input("Reenter the password: ")
+    
+    if pwd==cfp:
+      cfp_hash = hash_password(pwd)
+      users[usrname] = cfp_hash
+      save_users(users)
+      print("Thank you.")
+      lockscreen()
+      break
   else:
     print("PASSWORDS DO NOT MATCH")
-    login()
 
 def lockscreen():
   global cfp_hash
